d2e842646c12676427b3f03f876fd804ecff75fe
Rogelio AV <acostavilledar@gmail.com>
2024-12-16 16:07:14 -0600

    se crea el campo de telefono

--- JS/contactanos.js
+++ JS/contactanos.js
@@ -13,3 +13,20 @@ window.addEventListener('load', event => {
     event.preventDefault();
     txtMensaje.maxLength = caracteresRestantes;
 });
+
+function validarTelefono() {
+    const telefono = document.getElementById("telefono").value;
+    
+    if (telefono.length !== 10) {
+      document.getElementById("mensaje").innerHTML = "Número de teléfono inválido. Debe tener 10 dígitos.";
+      alert("Número de teléfono inválido. Debe tener 10 dígitos.");
+    } else if (telefono[0] === "0") {
+      document.getElementById("mensaje").innerHTML = "Número de teléfono inválido. El primer dígito no puede ser 0.";
+      alert("Número de teléfono inválido. El primer dígito no puede ser 0.");
+    } else if (!/^\d+$/.test(telefono)) {
+      document.getElementById("mensaje").innerHTML = "Número de teléfono inválido. Solo se permiten dígitos.";
+      alert("Número de teléfono inválido. Solo se permiten dígitos.");
+    } else {
+      document.getElementById("mensaje").innerHTML = "Número de teléfono válido";
+    }
+  }
