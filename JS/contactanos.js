969ee9270c6b3921a97139cd3188e5f9db6e5e78
E.CORNEJO <cornejohe2000@gmail.com>
2024-12-16 17:06:56 -0600

    se añade correo electronico

---
+++ JS/contactanos.js
@@ -0,0 +1,16 @@
+// const email = document.getElementById("correo")
+
+// formData.addEventListener("submit" e =>{
+//     e.preventDefault()
+//     let warnings = ""
+//     let entrar = false
+//      let regexEmail = /[^@ \t\r\n]+@[^@ \t\r\n]+\.[^@ \t\r\n]+/
+// if(nombre.value.length <6){
+//     warnings += `El nombre no es valido </br>`
+//     entrar = true
+// }
+// if(!regexEmail.test(email.value)){
+//     warnings +=`El email no es valido </br>`
+//     entrar = true
+// }
+// })
