570001e8b7893c66ee579da9ab22b3263d024dcd
Isaac Victoria <isaac127@live.com.mx>
2024-12-19 15:23:16 -0600

    Se elimina carpeta .vscode y se añade .gitignore

--- JS/general.js
+++ JS/general.js
@@ -1,13 +1,18 @@
 // No tocar este documento a menos que sea para estructuras generales, avisar al equipo primero
-document.body.insertAdjacentHTML('afterbegin', `<nav class="navbar navbar-expand-sm navbar-light bg-light" id="barra">
-        <div class="container-fluid">
-            <a class="navbar-brand" href="#"><img src="../IMG/logo.png" alt="logo" id="logo"></a>
-            <div>
-                <button type="button" class="btn-nav"><a href="../HTML/iniciarSesion.html">Iniciar sesión</a></button>
-                <button type="button" class="btn-nav"><a href="../HTML/registro.html">Registrarse</a></button>
-            </div>
-        </div>
-    </nav>`);
+
+const estructuraNav = `
+
+`;
+// const estructuraNav = `<nav class="navbar navbar-expand-sm navbar-light bg-light" id="barra">
+//         <div class="container-fluid">
+//             <a class="navbar-brand" href="#"></a>
+
+//             <div>
+//                 
+//             </div>
+//         </div>
+//     </nav>`;
+document.body.insertAdjacentHTML('afterbegin', estructuraNav);
 
 function get(id) {
     return document.getElementById(id);
