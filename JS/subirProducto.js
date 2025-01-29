3307e953696b5a6ab83a7e823927ab6591a0b77e
Rogelio AV <acostavilledar@gmail.com>
2025-01-28 22:45:53 -0600

    se muestra la imagen en lista de productos

--- JS/subirProducto.js
+++ JS/subirProducto.js
@@ -1,44 +1,75 @@
+let imagenUrl = ""; // Declarar una variable global
+
 document.getElementById("btnEnviar").addEventListener("click", function (e) {
-    e.preventDefault();
+  e.preventDefault();
+  const nombre = document.getElementById("productName").value.trim();
+  const descripcion = document.getElementById("productDescription").value.trim();
+  const categoria = get('productCategory').options[get('productCategory').selectedIndex];
+  const precio = document.getElementById("productPrice").value.trim();
+  const stock = document.getElementById("productStock").value.trim();
+  const imagenInput = document.getElementById("productImagen");
+  const alerta = document.getElementById("alerta");
+  const botonImagen = document.querySelector('#imageUpload');
+  const imagen = document.querySelector('#imagenProducto');
 
-    const nombre = document.getElementById("productName").value.trim();
-    const descripcion = document.getElementById("productDescription").value.trim();
-    const categoria = get('productCategory').options[get('productCategory').selectedIndex];
-    const precio = document.getElementById("productPrice").value.trim();
-    const stock = document.getElementById("productStock").value.trim();
-    const imagenInput = document.getElementById("productImagen");
-    const alerta = document.getElementById("alerta");
+  let widgetCloudinary = cloudinary.createUploadWidget({
+    cloudName: 'marketmexa',
+    uploadPreset: 'preset_market'
+  }, (err, result) => {
+    if (!err && result && result.event === 'success') {
+      imagen.src = result.info.secure_url;
+      imagen.style.width = "300px";
+      imagenUrl = result.info.secure_url;
+      
 
-    alerta.classList.add("d-none");
-    alerta.innerHTML = "";
+      // Guardar el producto en el local storage
+      const producto = {
+        "name": nombre,
+        "descripcion": descripcion,
+        "categoria": categoria.value,
+        "precio": parseFloat(precio),
+        "stock": parseInt(stock, 10),
+        "img": imagenUrl
+      };
 
-    let errores = [];
-    let imagen = null;
+      const productos = JSON.parse(localStorage.getItem('productos')) || [];
+      productos.push(producto);
+      localStorage.setItem('productos', JSON.stringify(productos));
 
-    if (nombre === "") errores.push("El nombre del producto es obligatorio.");
-    if (descripcion === "") errores.push("La descripción no puede estar vacía.");
-    if (get('productCategory').options[get('productCategory').selectedIndex].value == 0) errores.push("Debes de seleccionar una categoría.");
-    if (!precio || isNaN(precio) || parseFloat(precio) <= 0) errores.push("El precio debe ser un número mayor a 0.");
-    if (!stock || isNaN(stock) || parseInt(stock, 10) < 0) errores.push("El stock debe ser un número mayor o igual a 0.");
-
-    if (!get('imagenProducto').src) {
-        errores.push("Debe de seleccionarse una imagen para el producto.");
+      alerta.classList.remove("d-none", "alert-personalizada");
+      alerta.classList.add("alert-personalizada");
+      alerta.innerHTML = "¡Producto creado exitosamente!";
+      document.getElementById("productName").value = "";
+      document.getElementById("productDescription").value = "";
+      document.getElementById("productCategory").value = "";
+      document.getElementById("productPrice").value = "";
+      document.getElementById("productStock").value = "";
+      get('imagenProducto').src = "";
     }
-    /*
-    Esto es lo que estaba en el if anterior pero me marca errores
-    if (imagenInput && imagenInput.files.length > 0) {
-        const file = imagenInput.files[0];
-        imagen = URL.createObjectURL(file);
-    } else {
-        errores.push("Debe seleccionarse una imagen para el producto.");
-    }
-    */
+  });
 
-    if (errores.length > 0) {
-        alerta.classList.remove("d-none", "alert-personalizada");
-        alerta.classList.add("alert-personalizada");
-        alerta.innerHTML = errores.join("<br>");
-        return;
+  botonImagen.addEventListener('click', () => {
+    widgetCloudinary.open();
+  }, false);
+
+  alerta.classList.add("d-none");
+  alerta.innerHTML = "";
+  let errores = [];
+
+  if (nombre === "") errores.push("El nombre del producto es obligatorio.");
+  if (descripcion === "") errores.push("La descripción no puede estar vacía.");
+  if (get('productCategory').options[get('productCategory').selectedIndex].value == 0) errores.push("Debes de seleccionar una categoría.");
+  if (!precio || isNaN(precio) || parseFloat(precio) <= 0) errores.push("El precio debe ser un número mayor a 0.");
+  if (!stock || isNaN(stock) || parseInt(stock, 10) < 0) errores.push("El stock debe ser un número mayor o igual a 0.");
+  if (!get('imagenProducto').src) {
+    errores.push("Debe de seleccionarse una imagen para el producto.");
+  }
+
+  if (errores.length > 0) {
+    alerta.classList.remove("d-none", "alert-personalizada");
+    alerta.classList.add("alert-personalizada");
+    alerta.innerHTML = errores.join("<br>");
+    return;
     } else {
         alerta.classList.remove("d-none", "alert-personalizada");
         alerta.classList.add("alert-personalizada");
@@ -48,7 +79,7 @@ document.getElementById("btnEnviar").addEventListener("click", function (e) {
         document.getElementById("productCategory").value = "";
         document.getElementById("productPrice").value = "";
         document.getElementById("productStock").value = "";
-        get('imagenProducto').src = "";
+        //get('imagenProducto').src = "";
     }
     
     const producto = {
@@ -57,11 +88,14 @@ document.getElementById("btnEnviar").addEventListener("click", function (e) {
         "categoria": categoria.value,
         "precio": parseFloat(precio),
         "stock": parseInt(stock, 10),
-        "img": get('imagenProducto').src    // Aquí hace falta añadir el url creado por cloudinary
+        "img": imagen.src    // Aquí hace falta añadir el url creado por cloudinary
     };
     const productos = JSON.parse(localStorage.getItem('productos')) || [];
 
     productos.push(producto);
 
     localStorage.setItem('productos', JSON.stringify(productos));
+    imagenUrl = "";
+    document.getElementById("imagenProducto").src = "";
+  
 });
